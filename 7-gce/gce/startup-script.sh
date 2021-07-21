63c3661882c0fbac49b621196096b4b2bc2b3520
Illia_Bilinskyi <illia_bilinskyi@epam.com>
2021-07-21 20:38:40 +0000

    my changes9

--- 7-gce/gce/startup-script.sh
+++ 7-gce/gce/startup-script.sh
@@ -48,6 +48,8 @@ git clone https://github.com/awsivbilinskyy/getting-started-python.git -b steps 
 virtualenv -p python3 /opt/app/7-gce/env
 source /opt/app/7-gce/env/bin/activate
 /opt/app/7-gce/env/bin/pip install -r /opt/app/7-gce/requirements.txt
+#test
+/opt/app/7-gce/env/bin/python /opt/app/7-gce/bookshelf/model_cloudsql.py
 
 # Make sure the pythonapp user owns the application code
 chown -R pythonapp:pythonapp /opt/app
